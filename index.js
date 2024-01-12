15db0e270be5c89c3bb639fb1e275eef09c4c5b4
rhtm123 <maurya.iitk@gmail.com>
2024-01-12 12:51:32 +0530

    all models added

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 
 import express from 'express';
 import AdminJS from 'adminjs'
+
 import AdminJSExpress from '@adminjs/express'
 
 import mongoose from 'mongoose'
 import * as AdminJSMongoose from '@adminjs/mongoose'
 
 import 'dotenv/config'
+
+import Builder from "./models/builderModel.js";
+import Society from "./models/societyModel.js";
+import Wing from "./models/wingModel.js";
+import Flat from "./models/flatModel.js";
+
+import Member from "./models/memberModel.js";
+import Owner from "./models/ownerModel.js";
+import Tenant from "./models/tenantModel.js";
+import OwnerFamily from './models/ownerFamilyModel.js';
+import TenantFamily from './models/tenantFamilyModel.js';
+
+import Penalty from "./models/penaltyModel.js";
+import OwnerPenalty from "./models/ownerPenaltyModel.js";
+
 
 
 async function startServer(){
@@ -15,6 +31,33 @@ async function startServer(){
   try {
     databaseDB = await mongoose.connect(process.env.MONGODB_URI, { 
      });
+
+
+     AdminJS.registerAdapter({
+      Resource: AdminJSMongoose.Resource,
+      Database: AdminJSMongoose.Database,
+    })
+  
+    const admin = new AdminJS({
+      databases: [databaseDB],
+      resources:[
+        Builder,
+        Society,
+        Wing, 
+        Flat, 
+        Member, 
+        Owner, 
+        Tenant, 
+        OwnerFamily, 
+        TenantFamily,
+        Penalty, 
+        OwnerPenalty
+      ]
+    })
+  
+    const adminRouter = AdminJSExpress.buildRouter(admin);
+    app.use(admin.options.rootPath, adminRouter);
+
 
     console.log('Connected to MongoDB');
   } catch (error) {
@@ -27,80 +70,16 @@ async function startServer(){
   });
   
 
-  AdminJS.registerAdapter({
-    Resource: AdminJSMongoose.Resource,
-    Database: AdminJSMongoose.Database,
-  })
-
-  const admin = new AdminJS({
-    databases: [databaseDB],
-    // resources:[
-    //   Builder,
-    //   Society,
-    //   Wing,
-    //   Flat,
-    //   Member,
-    //   Owner,
-    //   OwnerFamily,
-      
-    //   Tenant,
-    //   TenantFamily,
   
-    //   Penalty,
-    //   OwnerPenalty,
-      
-    // ]
-  })
-
-  const adminRouter = AdminJSExpress.buildRouter(admin)
-  app.use(admin.options.rootPath, adminRouter)
 
   const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
+    console.log(`AdminJS started on http://localhost:${PORT}/admin`)
   });
-
 
 }
 
-// let mongoDB = connectDB();
-
 startServer();
-
-
-
-
-
-// const admin = new AdminJS({
-//   databases: [mongoose],
-//   resources:[
-//     Builder,
-//     Society,
-//     Wing,
-//     Flat,
-//     Member,
-//     Owner,
-//     OwnerFamily,
-    
-//     Tenant,
-//     TenantFamily,
-
-//     Penalty,
-//     OwnerPenalty,
-    
-//   ]
-// })
-
-// const adminRouter = AdminJSExpress.buildRouter(admin)
-// app.use(admin.options.rootPath, adminRouter)
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-//   console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
-// });
-
 
