dc47f7f209d642ec59966cca16fb8882874679cd
rhtm123 <maurya.iitk@gmail.com>
2024-01-25 17:23:45 +0530

    wingName added

--- models/flatModel.js
+++ models/flatModel.js
@@ -1,10 +1,13 @@
 // const mongoose = require('mongoose');
 import mongoose from "mongoose";
+import Wing from "./wingModel.js";
 
 const flatSchema = new mongoose.Schema({
   wingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Wing', required: true },
   name: { type: String, required: true },
+  wingName: { type: String},
   floorNumber: { type: Number },
+
   
   occupied: { type: Boolean, default: false },
   category: { type: String},
@@ -12,6 +15,25 @@ const flatSchema = new mongoose.Schema({
 
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
+});
+
+flatSchema.pre('save', async function (next) {
+  try {
+    // Assuming 'Wing' model is used
+    const wing = await Wing.findById(this.wingId);
+
+    if (wing) {
+      this.wingName = wing.name;
+    } else {
+      // Handle the case when the corresponding wing is not found
+      // You can throw an error, set a default value, or handle it in any way you prefer
+      throw new Error('Wing not found');
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const Flat = mongoose.model("Flat", flatSchema);
